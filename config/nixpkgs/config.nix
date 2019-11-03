1d85af0fd66cb4cb9cac201db7be3c723f3cede7
Graham Bennett <graham@grahambennett.org>
2019-11-03 13:33:20 +0000

    Updates.
    GitOrigin-RevId: a2b20d0f68e346c47df06271a19b4be6d546ce65

--- config/nixpkgs/config.nix
+++ config/nixpkgs/config.nix
@@ -1,16 +1,32 @@
+{pkgs}:
+let
+  myCustomWhitelist = with pkgs.stdenv.lib.licenses; [ artistic1 bsd3 gpl2 gpl3 gpl2Plus gpl3Plus lgpl21 lgpl2Plus lgpl21Plus zlib ];
+in
 {
   allowUnfree = true;
+
+  #licenseCheckPredicate = attrs: attrs ? name && attrs.name == "pcre-8.43";
+
+  # blacklistedLicenses = with pkgs.stdenv.lib.licenses; [ agpl3Plus ];
 
   packageOverrides = pkgs: with pkgs; {
     myPackages = pkgs.buildEnv {
       name = "my-packages";
       paths = [
+        awscli
         bat
         emacs
+        freerdp
+        google-chrome
         lxterminal
+        nodejs
         ripgrep
         tmux
-      ];
+      ] ++ (with nodePackages; [
+        create-react-app
+        serverless
+      ]);
     };
   };
+
 }
