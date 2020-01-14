4ec6e9773149e87e65a2b51edc30ae8411c77f88
Graham Bennett <graham@grahambennett.org>
2020-01-14 00:14:29 +0000

    Updates
    GitOrigin-RevId: 465fb471e86dd96bd538df061c14aa3d400a4c26

--- config/nixpkgs/config.nix
+++ config/nixpkgs/config.nix
@@ -1,7 +1,11 @@
 {pkgs}:
 let
-  myPython = [(pkgs.python3.withPackages (ps: with ps; [
+  myPython2 = [(pkgs.python2.withPackages (ps: with ps; [
+    boto
+  ]))];
+  myPython3 = [(pkgs.python3.withPackages (ps: with ps; [
     boto3
+    dbus-python
     numpy
     pandas
   ]))];
@@ -19,17 +23,23 @@ in
       paths = [
         awscli
         bat
+        dnsutils
         emacs
+        exiftool
         freerdp
+        gimp
         google-chrome
+        imagemagick
         lxterminal
         nodejs
+	remmina
         ripgrep
         tmux
+        unzip
       ] ++ (with nodePackages; [
         create-react-app
         serverless
-      ]) ++ myPython;
+      ]) ++ myPython2 ++ myPython3;
     };
   };
 
