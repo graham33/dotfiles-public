7c5fb6d58685b8078262427f50e287cbf06530c1
Graham Bennett <graham@grahambennett.org>
2022-10-03 22:31:00 +0100

    Add home-manager config
    GitOrigin-RevId: 871aa7e163145f259a0e9fc1b9f01478cf9b27f3

---
+++ config/nixpkgs/home.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... }:
+
+{
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "graham";
+  home.homeDirectory = "/home/graham";
+
+  programs.git = {
+    enable = true;
+    userName = "Graham Bennett";
+    userEmail = "graham@grahambennett.org";
+    extraConfig = {
+      pull = { rebase = "true"; };
+      push = { default = "simple"; };
+    };
+  };
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "21.05";
+}
