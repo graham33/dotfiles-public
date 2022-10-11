1080b36637ddec98704fb527e10a14f97066ff25
Graham Bennett <graham@grahambennett.org>
2022-10-11 21:49:34 +0100

    lxterminal
    GitOrigin-RevId: 623355d3039919826dfe6e34c7e8168f28a1f792

--- config/nixpkgs/home.nix
+++ config/nixpkgs/home.nix
@@ -245,6 +245,8 @@
     };
   };
 
+  xdg.configFile."lxterminal/lxterminal.conf".source = ./config/lxterminal/lxterminal.conf;
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
