cb11a5346296cab757954f1422fba82d753aba15
Graham Bennett <graham@grahambennett.org>
2022-10-12 20:30:19 +0100

    Clean up nixpkgs config
    GitOrigin-RevId: 8ce93bc3b5032fd2f7837b8f933788a15471201e

--- config/nixpkgs/nixpkgs-config.nix
+++ config/nixpkgs/nixpkgs-config.nix
@@ -1,16 +1,4 @@
 {pkgs}:
 {
   allowUnfree = true;
-
-  packageOverrides = pkgs: {
-    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
-      inherit pkgs;
-    };
-  };
-
-  permittedInsecurePackages = [
-    "adobe-reader-9.5.5"
-    "xpdf-4.02"
-  ];
-
 }
