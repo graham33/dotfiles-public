ac2b87188d62cfa4d62ed853ad1b4e8a47b8a7c2
Graham Bennett <graham@grahambennett.org>
2024-02-18 14:08:07 +0000

    Remove i3 and lxterminal
    GitOrigin-RevId: 26e64c975df43c1da192a4bcb4bfe206085340ad

--- home.nix
+++ home.nix
@@ -128,17 +128,6 @@
     enable = true;
   };
   programs.htop.enable = true;
-  programs.i3status = {
-    enable = true;
-    modules = {
-      "volume master" = {
-        position = 1;
-        settings = {
-          device = "pulse";
-        };
-      };
-    };
-  };
   programs.jq.enable = true;
   programs.kitty = {
     enable = true;
@@ -281,7 +270,6 @@
     inkscape
     killall
     libreoffice
-    lxterminal
     mpv
     myPython3
     nix-output-monitor
@@ -428,97 +416,10 @@
   };
   services.mako.enable = true;
 
-  xsession.windowManager.i3 = let
-    modifier = config.xsession.windowManager.i3.config.modifier;
-    terminal = config.xsession.windowManager.i3.config.terminal;
-  in {
-    enable = true;
-    config = {
-      assigns = {
-        "1: tmux" = [{ class = "kitty"; title = "tmux"; }];
-        "2: web" = [{class = "Google-chrome"; }];
-      };
-      workspaceOutputAssign = [
-        { workspace = "1: tmux"; output = "primary"; }
-        { workspace = "2: web"; output = "nonprimary"; }
-      ];
-      bars = [{
-        statusCommand = "${pkgs.i3status}/bin/i3status";
-        fonts = {
-          names = [ "Source Code Pro" ];
-          size = 11.0;
-        };
-      }];
-      fonts = {
-        names = [ "Source Code Pro" ];
-        size = 11.0;
-      };
-      keybindings = let
-        refresh_i3status = "killall -SIGUSR1 i3status";
-      in lib.mkOptionDefault {
-        # movement
-        "${modifier}+Shift+j" = "move left";
-        "${modifier}+Shift+k" = "move down";
-        "${modifier}+Shift+l" = "move up";
-        "${modifier}+Shift+semicolon" = "move right";
-        # lock screen
-        "${modifier}+Shift+s" = "exec --no-startup-id i3lock";
-        # Use pactl to adjust volume in PulseAudio.
-        XF86AudioRaiseVolume = "exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ +10% && ${refresh_i3status}";
-        XF86AudioLowerVolume = "exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ -10% && ${refresh_i3status}";
-        XF86AudioMute = "exec --no-startup-id pactl set-sink-mute @DEFAULT_SINK@ toggle && ${refresh_i3status}";
-        XF86AudioMicMute = "exec --no-startup-id pactl set-source-mute @DEFAULT_SOURCE@ toggle && ${refresh_i3status}";
-        # Screen brightness controls
-        XF86MonBrightnessUp = "exec xbright +0.1 # increase screen brightness";
-        XF86MonBrightnessDown = "exec xbright -0.1 # decrease screen brightness";
-        # Cycle autorandr through detected configurations
-        XF86Display = "exec --no-startup-id autorandr --cycle";
-      };
-      modifier = "Mod4";
-      startup = [
-        { command = "${pkgs.xss-lock}/bin/xss-lock --transfer-sleep-lock -- i3lock --nofork"; notification = false; }
-        { command = "nm-applet"; notification = false; }
-        { command = "dropbox start"; notification = false; }
-        # screensaver after 20 mins
-        { command = "xset s 1200"; }
-        # screen standby after 30 mins, turn off after 1hr
-        { command = "xset dpms 1800 1800 3600"; }
-        # Start terminal with tmux
-        { command = "${terminal} --title tmux tmux"; }
-        # Start google-chrome
-        { command = "google-chrome-stable"; }
-      ];
-      terminal = "kitty";
-    };
-    extraConfig = ''
-      set $Locker i3lock && sleep 1
-
-      set $mode_system System (l) lock, (e) logout, (s) suspend, (h) hibernate, (r) reboot, (Shift+s) shutdown
-      mode "$mode_system" {
-          bindsym l exec --no-startup-id $Locker, mode "default"
-          bindsym e exec --no-startup-id i3-msg exit, mode "default"
-          bindsym s exec --no-startup-id $Locker && systemctl suspend, mode "default"
-          bindsym h exec --no-startup-id $Locker && systemctl hibernate, mode "default"
-          bindsym r exec --no-startup-id systemctl reboot, mode "default"
-          bindsym Shift+s exec --no-startup-id systemctl poweroff -i, mode "default"
-
-          # back to normal: Enter or Escape
-          bindsym Return mode "default"
-          bindsym Escape mode "default"
-      }
-
-      bindsym ${modifier}+Shift+Home mode "$mode_system"
-
-      # switch to workspace 1
-      exec --no-startup-id i3-msg workspace "1: tmux"
-    '';
-  };
-
   home.file.".octaverc".source = ./octaverc;
   xdg.configFile."doom/config.el".source = ./config/doom/config.el;
   xdg.configFile."doom/init.el".source = ./config/doom/init.el;
   xdg.configFile."doom/packages.el".source = ./config/doom/packages.el;
-  xdg.configFile."lxterminal/lxterminal.conf".source = ./config/lxterminal/lxterminal.conf;
   xdg.configFile."vlc/vlcrc".source = ./config/vlc/vlcrc;
   xdg.configFile."zoomus.conf".source = ./config/zoomus.conf;
 
